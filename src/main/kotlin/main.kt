c66f05838a375a21692543d87ff10b2db910e3aa
Anton G <il427221@gmail.com>
2025-12-04 18:41:21 +0300

    Applying string template

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,10 @@
 fun main() {
     val likes = 1201
-    val human = "человеку"
-    val people = "людям"
     val lastDigit = likes % 10
     val lastTwoDigits = likes % 100
     if ((lastTwoDigits < 11 || lastTwoDigits > 19) && lastDigit == 1) {
-        println ("«Понравилось " + likes + " " + human + "».")
+        println("«Понравилось $likes человеку».")
     } else {
-        println ("«Понравилось " + likes + " " + people + "».")
+        println("«Понравилось $likes людям».")
     }
 }
